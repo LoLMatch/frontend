bf4435628e03129052df4b1281b143355648d21a
Dawid Szczepankowski <dawidszczepankowski@MacBook-Air-Dawid.local>
2023-11-09 20:52:31 +0100

    Added stadnalone to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ds-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'angular-template';
