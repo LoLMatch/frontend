71babaebf05738276fa2f6ded419375ea45dc885
Dawid Szczepankowski <57863833+Alokin1112@users.noreply.github.com>
2023-11-26 23:21:40 +0100

    Update register-form-creator.service.ts

--- src/app/pages/auth/services/registerFormCreator/register-form-creator.service.ts
+++ src/app/pages/auth/services/registerFormCreator/register-form-creator.service.ts
@@ -7,12 +7,12 @@ export class RegisterFormCreatorService {
 
   constructor(private fb: FormBuilder) { }
 
-  createForm(){
+  createForm() {
     return this.fb.group({
-      name: [null, [Validators.required, Validators.maxLength(255)]],
-      email: [null, [Validators.email, Validators.required, Validators.maxLength(255)]],
-      password: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(255)]],
-      repeatPassword: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(255)]]
-    }, {validators: [PasswordValidator]});
+      name: [null as string, [Validators.required, Validators.maxLength(255)]],
+      email: [null as string, [Validators.email, Validators.required, Validators.maxLength(255)]],
+      password: [null as string, [Validators.required, Validators.minLength(8), Validators.maxLength(255)]],
+      repeatPassword: [null as string, [Validators.required, Validators.minLength(8), Validators.maxLength(255)]]
+    }, { validators: [PasswordValidator] });
   }
 }
